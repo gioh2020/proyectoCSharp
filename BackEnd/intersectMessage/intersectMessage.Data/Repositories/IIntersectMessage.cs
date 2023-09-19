c7740e8b227355290805d796d986f0863b565652
Gioh <gioh2020@gmail.com>
2023-09-18 21:51:20 -0500

    devGioH/Primer commit

---
+++ BackEnd/intersectMessage/intersectMessage.Data/Repositories/IIntersectMessage.cs
@@ -0,0 +1,15 @@
+﻿using intersectMessage.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace intersectMessage.Data.Repositories
+{
+    internal interface IIntersectMessage
+    {
+        Task<IEnumerable<Sato>> GetAllMesage();
+    }
+}
