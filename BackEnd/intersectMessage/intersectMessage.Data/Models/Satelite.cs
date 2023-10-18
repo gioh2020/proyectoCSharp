268740135a143037b7a6fa0cfadc1ea6e0f43270
Gioh <gioh2020@gmail.com>
2023-10-17 22:20:46 -0500

    devGio/modificar/back/ se crea el modelo satelite

---
+++ BackEnd/intersectMessage/intersectMessage.Data/Models/Satelite.cs
@@ -0,0 +1,11 @@
+﻿namespace intersectMessage
+{
+    public class Satelite
+    {
+        public int SateliteId { get; set; }
+        public int SateliteName { get; set;}
+        public int Coordenadax { get; set;}
+        public int Coordenaday { get; set; }
+
+    }
+}
