dd81a99e1db0052c8d80693e5bbb1485ecbd8222
Gioh <gioh2020@gmail.com>
2023-10-20 23:44:19 -0500

    devGioh/back/crear/se creo el modelo MessageIntersectAndSatelites

--- BackEnd/intersectMessage/intersectMessage.Data/Models/Satelite.cs
+++ BackEnd/intersectMessage/intersectMessage.Data/Models/Satelite.cs
@@ -3,7 +3,7 @@
     public class Satelite
     {
         public int SateliteId { get; set; }
-        public int SateliteName { get; set;}
+        public string SateliteName { get; set;}
         public int Coordenadax { get; set;}
         public int Coordenaday { get; set; }
 
