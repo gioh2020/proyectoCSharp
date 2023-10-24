091d43b8401b06c999fbe80c943ae0490b822a2c
Gioh <gioh2020@gmail.com>
2023-10-23 20:49:46 -0500

    devGio/modificar/back se agrega el modelo MessageIntersectAndSatelites

--- BackEnd/intersectMessage/intersectMessage.Data/Models/MessageIntersectAndSatelites.cs
+++ BackEnd/intersectMessage/intersectMessage.Data/Models/MessageIntersectAndSatelites.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace intersectMessage.Data.Models
 {
     public class MessageIntersectAndSatelites
     {
-        public list<Satelite> Satelites { get; set; }
-        public list<MessageIntersect> Messages{get; set; }
+ 
+        public List<Satelite> Satelites { get; set; }
+        public List<MessageIntersect> Messages{get; set; }
     }
 }
